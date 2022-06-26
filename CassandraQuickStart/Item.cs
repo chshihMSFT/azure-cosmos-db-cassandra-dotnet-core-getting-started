0ff87b3f41b52cee5dce0df07059f959c8a82f4c
Ethan <ChingYuan.Shih@microsoft.com>
2022-06-26 09:05:03 +0800

    * adding more controls of insertion and table mapping * move configurations to AppSettings * bump up driver to the latest (3.13.0 => 3.17.1)

---
+++ CassandraQuickStart/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CassandraQuickStart
+{
+    /**
+     * User Item entity class
+     */
+    public class Item
+    {
+        public String item_category { get; set; }
+        public String item_id { get; set; }
+        public String item_name { get; set; }
+        public String item_createTime { get; set; }
+
+        public Item(String item_category, String item_id, String item_name, String item_createTime)
+        {
+            this.item_category = item_category;
+            this.item_id = item_id;
+            this.item_name = item_name;
+            this.item_createTime = item_createTime;
+        }
+        public override String ToString()
+        {
+            return String.Format(" {0} | {1} | {2} | {3} ", item_category, item_id, item_name, item_createTime);
+        }
+    }
+}
--- CassandraQuickStart/Program.cs
+++ CassandraQuickStart/Program.cs
@@ -5,20 +5,58 @@ using System.Net.Security;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
 
 namespace CassandraQuickStart
 {
 	public class Program
 	{
 		// Cassandra Cluster Configs      
-		private const string USERNAME = "<PROVIDE>";
-		private const string PASSWORD = "<PROVIDE>";
-		private const string CASSANDRACONTACTPOINT = "<PROVIDE>.cassandra.cosmos.azure.com";  // DnsName  
-		private static int CASSANDRAPORT = 10350;
+		private static string UserName;
+		private static string Password;
+		private static string CassandraContactPoint;
+		private static int CassandraPort;
+
+		private static string keyspaceName;
+		private static string tableName;
+
+		private static int InsertTotalItems;
+		private static int InsertItemsPerBatch;
+		private static int InsertDelayMSPerBatch;
 
 		static void Main(string[] args)
 		{
-			ProcessAsync().Wait();
+			//Reading application settings
+			try
+			{
+				IConfigurationRoot configuration = new ConfigurationBuilder()
+							.AddJsonFile("appSettings.json")
+							.Build();
+
+				UserName = configuration["UserName"];
+				Password = configuration["Password"];
+				CassandraContactPoint = configuration["CassandraContactPoint"];
+				CassandraPort = Int32.Parse(configuration["CassandraPort"]);
+
+				keyspaceName = configuration["keyspaceName"];
+				tableName = configuration["tableName"];
+
+				InsertTotalItems = Int32.Parse(configuration["InsertTotalItems"]);
+				InsertItemsPerBatch = Int32.Parse(configuration["InsertItemsPerBatch"]);
+				InsertDelayMSPerBatch = Int32.Parse(configuration["InsertDelayMSPerBatch"]);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, reading application settings, failed: {ex.Message}");
+			}
+			finally
+			{
+				Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, demo start");
+				ProcessAsync().Wait();
+				Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, demo end");
+			}
 
 			Console.WriteLine("Done! You can go look at your data in Cosmos DB. Don't forget to clean up Azure resources!");
 			Console.WriteLine("Press any key to exit.");
@@ -31,36 +69,73 @@ namespace CassandraQuickStart
 			// Connect to cassandra cluster  (Cassandra API on Azure Cosmos DB supports only TLSv1.2)
 			var options = new Cassandra.SSLOptions(SslProtocols.Tls12, true, ValidateServerCertificate);
 
-			options.SetHostNameResolver((ipAddress) => CASSANDRACONTACTPOINT);
+			options.SetHostNameResolver((ipAddress) => CassandraContactPoint);
 			Cluster cluster = Cluster
 				.Builder()
-				.WithCredentials(USERNAME, PASSWORD)
-				.WithPort(CASSANDRAPORT)
-				.AddContactPoint(CASSANDRACONTACTPOINT)
+				.WithCredentials(UserName, Password)
+				.WithPort(CassandraPort)
+				.AddContactPoint(CassandraContactPoint)
 				.WithSSL(options)
 				.Build()
 			;
 
 			ISession session = await cluster.ConnectAsync();
+			Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, connected to Cassandra cluster: {CassandraContactPoint}");
 
 			// Creating KeySpace and table
-			await session.ExecuteAsync(new SimpleStatement("DROP KEYSPACE IF EXISTS uprofile"));
-			await session.ExecuteAsync(new SimpleStatement("CREATE KEYSPACE uprofile WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 };"));
-			Console.WriteLine(String.Format("created keyspace uprofile"));
-			await session.ExecuteAsync(new SimpleStatement("CREATE TABLE IF NOT EXISTS uprofile.user (user_id int PRIMARY KEY, user_name text, user_bcity text)"));
-			Console.WriteLine(String.Format("created table user"));
+			String cqlCreateKeySpace = "CREATE KEYSPACE IF NOT EXISTS " + keyspaceName + " WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', 'datacenter1' : 1 }; ";
+			await session.ExecuteAsync(new SimpleStatement(cqlCreateKeySpace));
+			Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, {cqlCreateKeySpace}");
+			String cqlCreatTable = "CREATE TABLE IF NOT EXISTS " + keyspaceName + "." + tableName + " (item_category varchar, item_id varchar, item_name varchar, item_createtime varchar, PRIMARY KEY(item_category, item_id)); ";
+			await session.ExecuteAsync(new SimpleStatement(cqlCreatTable));
+			Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, {cqlCreatTable}");
 
-			session = await cluster.ConnectAsync("uprofile");
+			session = await cluster.ConnectAsync(keyspaceName);
 			IMapper mapper = new Mapper(session);
 
-			// Inserting Data into user table
-			await mapper.InsertAsync<User>(new User(1, "LyubovK", "Dubai"));
-			await mapper.InsertAsync<User>(new User(2, "JiriK", "Toronto"));
-			await mapper.InsertAsync<User>(new User(3, "IvanH", "Mumbai"));
-			await mapper.InsertAsync<User>(new User(4, "LiliyaB", "Seattle"));
-			await mapper.InsertAsync<User>(new User(5, "JindrichH", "Buenos Aires"));
-			Console.WriteLine("Inserted data into user table");
+			//Set the Mapping Configuration
+			MappingConfiguration.Global.Define(
+				new Map<Item>()
+					.TableName(tableName)
+					.PartitionKey(u => u.item_category, u => u.item_id)
+			);
 
+			// Inserting Data into table
+			var DemoPK = "demo" + DateTime.UtcNow.ToString("yyyyMMddHHmmss");
+			int AccumulatedItems = 0;
+			int BatchCount = 1;
+			int SerialNo = 1;
+			while (AccumulatedItems < InsertTotalItems)
+			{
+				InsertItemsPerBatch = ((AccumulatedItems + InsertItemsPerBatch) > InsertTotalItems) ? (InsertTotalItems - AccumulatedItems) : InsertItemsPerBatch;
+				for (int i = 1; i <= InsertItemsPerBatch; i++)
+				{
+					try
+					{
+						var newitem = new Item(
+								DemoPK																	//item_category
+								, String.Format($"{BatchCount.ToString().PadLeft(4, '0')}-{i.ToString().PadLeft(6, '0')}-{SerialNo++.ToString().PadLeft(10, '0')}")	//item_id
+								, Guid.NewGuid().ToString().Substring(0, 8)								//item_name
+								, DateTime.UtcNow.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'ffffff'Z'")	//item_createtime
+							);
+
+						await mapper.InsertAsync<Item>(newitem);
+						Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, Insert:{{{newitem.ToString()}}}");
+						AccumulatedItems++;
+					}
+					catch (Exception ce)
+					{
+						Console.WriteLine($"{DateTime.UtcNow.ToString("yyyy-MM-dd HH:mm:ss.ffffff")}, Insert failed:{ce.Message}");
+					}
+					finally
+					{
+						Thread.Sleep(InsertDelayMSPerBatch);
+					}
+				}
+				BatchCount++;
+			}
+
+			/*
 			Console.WriteLine("Select ALL");
 			Console.WriteLine("-------------------------------");
 			foreach (User user in await mapper.FetchAsync<User>("Select * from user"))
@@ -72,6 +147,7 @@ namespace CassandraQuickStart
 			Console.WriteLine("-------------------------------");
 			User userId3 = await mapper.FirstOrDefaultAsync<User>("Select * from user where user_id = ?", 3);
 			Console.WriteLine(userId3);
+			*/
 
 			// Clean up of Table and KeySpace - commented out since other QuickStarts do not immediately delete what was done,
 			// and so QuickStart user (that's you) has chance to go look at this data in Cosmos DB
